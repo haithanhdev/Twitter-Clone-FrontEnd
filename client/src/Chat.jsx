92ec22a4a16d576489df4abfd4c4eaf1612a3003
Nguyen Hai Thanh <edu.haithanh.nguyen1001@gmail.com>
2025-03-19 12:34:53 +0700

    Chat between 2 client

--- client/src/Chat.jsx
+++ client/src/Chat.jsx
@@ -1,22 +1,102 @@
-import { useEffect } from "react";
-import { io } from "socket.io-client";
+import { useEffect, useState } from "react";
+import socket from "./socket";
+import axios from "axios";
 
+const profile = JSON.parse(localStorage.getItem("profile"));
+const usernames = [
+  {
+    name: "user1",
+    value: "user67d7a1ebac503a0d55c01448",
+  },
+  {
+    name: "user2",
+    value: "user67d7a455ac503a0d55c0144a",
+  },
+];
 export default function Chat() {
-  useEffect(() => {
-    const socket = io(import.meta.env.VITE_API_URL);
-    socket.on("connect", () => {
-      console.log(socket.id);
-      socket.emit("hello", "Tôi là bi Nguyễn");
-      socket.on("hi", (data) => {
-        console.log(data);
+  const [value, setValue] = useState("");
+  const [messages, setMessages] = useState([]);
+  const [receiver, setReceiver] = useState("");
+  const getProfile = async (username) => {
+    await axios
+      .get(`/users/${username}`, {
+        baseURL: import.meta.env.VITE_API_URL,
+      })
+      .then((res) => {
+        setReceiver(res.data.result._id);
+        alert(`Now you can chat with ${res.data.result.name}`);
       });
-    });
-    socket.on("disconnect", () => {
-      console.log(socket.id); // undefined
+  };
+  useEffect(() => {
+    socket.auth = {
+      _id: profile._id,
+    };
+    socket.connect();
+    socket.on("receiver private message", (data) => {
+      const content = data.content;
+      setMessages((messages) => [
+        ...messages,
+        {
+          content,
+          isSender: false,
+        },
+      ]);
     });
     return () => {
       socket.disconnect();
     };
   }, []);
-  return <div>Chat</div>;
+
+  const send = (e) => {
+    e.preventDefault();
+    setValue("");
+    socket.emit("private message", {
+      content: value,
+      to: receiver, //user_id
+    });
+    setMessages((messages) => [
+      ...messages,
+      {
+        content: value,
+        isSender: true,
+      },
+    ]);
+  };
+  return (
+    <div>
+      <h1>Chat</h1>
+      <div>
+        {usernames.map((username) => (
+          <div key={username.name}>
+            <button onClick={() => getProfile(username.value)}>
+              {username.name}
+            </button>
+          </div>
+        ))}
+      </div>
+      <div className="chat">
+        {messages.map((message, index) => (
+          <div key={index}>
+            <div className="message-container">
+              <div
+                className={
+                  "message " + (message.isSender ? "message-right" : "")
+                }
+              >
+                {message.content}
+              </div>
+            </div>
+          </div>
+        ))}
+      </div>
+      <form onSubmit={send}>
+        <input
+          type="text"
+          value={value}
+          onChange={(e) => setValue(e.target.value)}
+        />
+        <button type="submit">Send</button>
+      </form>
+    </div>
+  );
 }
