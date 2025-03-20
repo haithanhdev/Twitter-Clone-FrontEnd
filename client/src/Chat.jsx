c1eb3e8fc73a7a17faddc7ac691910b9fdba62c7
Nguyen Hai Thanh <edu.haithanh.nguyen1001@gmail.com>
2025-03-20 14:30:41 +0700

    Infinite scroll

--- client/src/Chat.jsx
+++ client/src/Chat.jsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
 import socket from "./socket";
 import axios from "axios";
+import InfiniteScroll from "react-infinite-scroll-component";
 
 const profile = JSON.parse(localStorage.getItem("profile"));
 const usernames = [
@@ -13,10 +14,16 @@ const usernames = [
     value: "user67d7a455ac503a0d55c0144a",
   },
 ];
+const LIMIT = 10;
+const PAGE = 1;
 export default function Chat() {
   const [value, setValue] = useState("");
   const [conversations, setConversations] = useState([]);
   const [receiver, setReceiver] = useState("");
+  const [pagination, setPagination] = useState({
+    page: PAGE,
+    total_page: 0,
+  });
   const getProfile = async (username) => {
     await axios
       .get(`/users/${username}`, {
@@ -35,7 +42,7 @@ export default function Chat() {
     socket.on("receive_message", (data) => {
       const { payload } = data;
       //Thêm vào array
-      setConversations((conversations) => [...conversations, payload]);
+      setConversations((conversations) => [payload, ...conversations]);
     });
     return () => {
       socket.disconnect();
@@ -51,15 +58,44 @@ export default function Chat() {
             Authorization: `Bearer ${localStorage.getItem("access_token")}`,
           },
           params: {
-            limit: 10,
-            page: 1,
+            limit: LIMIT,
+            page: PAGE,
           },
         })
         .then((res) => {
-          setConversations(res.data.result.conversations);
+          const { conversations, page, total_page } = res.data.result;
+          setConversations(conversations);
+          setPagination({
+            page,
+            total_page,
+          });
         });
     }
   }, [receiver]);
+
+  const fetchMoreConversations = () => {
+    if (receiver && pagination.page < pagination.total_page) {
+      axios
+        .get(`/conversations/receivers/${receiver}`, {
+          baseURL: import.meta.env.VITE_API_URL,
+          headers: {
+            Authorization: `Bearer ${localStorage.getItem("access_token")}`,
+          },
+          params: {
+            limit: LIMIT,
+            page: pagination.page + 1,
+          },
+        })
+        .then((res) => {
+          const { conversations, page, total_page } = res.data.result;
+          setConversations((prev) => [...prev, ...conversations]);
+          setPagination({
+            page,
+            total_page,
+          });
+        });
+    }
+  };
 
   const send = (e) => {
     e.preventDefault();
@@ -74,11 +110,11 @@ export default function Chat() {
       payload: conversation,
     });
     setConversations((conversations) => [
-      ...conversations,
       {
         ...conversation,
         _id: new Date().getTime(),
       },
+      ...conversations,
     ]);
   };
   return (
@@ -93,7 +129,45 @@ export default function Chat() {
           </div>
         ))}
       </div>
-      <div className="chat">
+      <div
+        id="scrollableDiv"
+        style={{
+          height: 300,
+          overflow: "auto",
+          display: "flex",
+          flexDirection: "column-reverse",
+        }}
+      >
+        {/*Put the scroll bar always on the bottom*/}
+        <InfiniteScroll
+          dataLength={conversations.length}
+          next={fetchMoreConversations}
+          style={{ display: "flex", flexDirection: "column-reverse" }} //To put endMessage and loader to the top.
+          inverse={true} //
+          hasMore={pagination.page < pagination.total_page}
+          loader={<h4>Loading...</h4>}
+          scrollableTarget="scrollableDiv"
+        >
+          {conversations.map((conversation) => (
+            <div key={conversation._id}>
+              <div className="message-container">
+                <div
+                  className={
+                    "message " +
+                    (conversation.sender_id === profile._id
+                      ? "message-right"
+                      : "")
+                  }
+                >
+                  {conversation.content}
+                </div>
+              </div>
+            </div>
+          ))}
+        </InfiniteScroll>
+      </div>
+
+      {/* <div className="chat">
         {conversations.map((conversation) => (
           <div key={conversation._id}>
             <div className="message-container">
@@ -110,7 +184,7 @@ export default function Chat() {
             </div>
           </div>
         ))}
-      </div>
+      </div> */}
       <form onSubmit={send}>
         <input
           type="text"
